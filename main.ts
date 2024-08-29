56ef01a7321b24d29b6f11693e50dc8eb95e19b6
Oskar0215 <karloskaryang@gmail.com>
2024-08-29 17:10:10 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,36 @@
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(receivedNumber)
     myControl += receivedNumber
+})
+input.onButtonPressed(Button.A, function () {
+    myControl = 1
+    basic.showNumber(1)
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M1,
+    255,
+    SuperBit.enMotors.M2,
+    255
+    )
+    basic.pause(1000)
+    myControl = 0
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(2)
+    myControl = 8
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M1,
+    -255,
+    SuperBit.enMotors.M2,
+    -255
+    )
+    basic.pause(1000)
+    myControl = 0
+})
+let myControl = 0
+radio.setGroup(1)
+myControl = 0
+SuperBit.RGB_Program().setBrightness(128)
+basic.forever(function () {
     if (myControl == 1) {
         SuperBit.MotorRunDual(
         SuperBit.enMotors.M1,
@@ -34,39 +64,4 @@ radio.onReceivedNumber(function (receivedNumber) {
         SuperBit.MotorStopAll()
         myControl = 0
     }
-})
-input.onButtonPressed(Button.A, function () {
-    myControl = 1
-    basic.showNumber(1)
-    SuperBit.MotorRunDual(
-    SuperBit.enMotors.M1,
-    255,
-    SuperBit.enMotors.M2,
-    255
-    )
-    basic.pause(1000)
-    myControl = 0
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(2)
-    myControl = 8
-    SuperBit.MotorRunDual(
-    SuperBit.enMotors.M1,
-    -255,
-    SuperBit.enMotors.M2,
-    -255
-    )
-    basic.pause(1000)
-    myControl = 0
-})
-let myControl = 0
-radio.setGroup(1)
-myControl = 0
-SuperBit.RGB_Program().setBrightness(128)
-basic.forever(function () {
-    SuperBit.RGB_Program().setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-    SuperBit.RGB_Program().setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-    SuperBit.RGB_Program().setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow))
-    SuperBit.RGB_Program().setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-    SuperBit.RGB_Program().show()
 })
